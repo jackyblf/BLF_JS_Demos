aed7c8887e45594d254631125704775570702820
jackyblf <47178234@qq.com>
2017-07-26 17:00:41 +0800

    Signed-off-by: jackyblf <47178234@qq.com>

--- BLFLib.js
+++ BLFLib.js
@@ -4,7 +4,7 @@
 
 // static util function defined here
 /*************************************Util Function Begin***************************************************/
-var BLFUtil = function(){};
+var BLFUtil = function() {};
 BLFUtil.inherits = function(ctor, superCtor) {
     if (ctor === undefined || ctor === null)
         throw new TypeError('The constructor to "inherits" must not be ' +
@@ -24,67 +24,61 @@ BLFUtil.inherits = function(ctor, superCtor) {
 /*************************************Util Function End*****************************************************/
 
 /*************************************Render Begin***************************************************/
-function BLFRender(context)
-{
-    if(!(this instanceof BLFRender))
+function BLFRender(context) {
+    if (!(this instanceof BLFRender))
         return new BLFRender();
 
     this.context = context;
 }
 
-BLFRender.prototype.drawGrid = function(backcolor,color, stepx, stepy)
-{
-    context = this.context;
+BLFRender.prototype.drawGrid = function(backcolor, color, stepx, stepy) {
+        context = this.context;
 
-    context.save()
+        context.save()
 
-    context.strokeStyle = color;
-    context.fillStyle = backcolor;
-    context.lineWidth = 0.5;
-    context.fillRect(0, 0, context.canvas.width, context.canvas.height);
-    context.globalAlpha = 0.1;
+        context.strokeStyle = color;
+        context.fillStyle = backcolor;
+        context.lineWidth = 0.5;
+        context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+        context.globalAlpha = 0.5;
 
-    context.beginPath();
-    for (var i = stepx + 0.5; i < context.canvas.width; i += stepx) {
-        context.moveTo(i, 0);
-        context.lineTo(i, context.canvas.height);
+        context.beginPath();
+        for (var i = stepx + 0.5; i < context.canvas.width; i += stepx) {
+            context.moveTo(i, 0);
+            context.lineTo(i, context.canvas.height);
+        }
+        context.stroke();
+
+        context.beginPath();
+        for (var i = stepy + 0.5; i < context.canvas.height; i += stepy) {
+            context.moveTo(0, i);
+            context.lineTo(context.canvas.width, i);
+        }
+        context.stroke();
+
+        context.restore();
     }
-    context.stroke();
-
-    context.beginPath();
-    for (var i = stepy + 0.5; i < context.canvas.height; i += stepy) {
-        context.moveTo(0, i);
-        context.lineTo(context.canvas.width, i);
-    }
-    context.stroke();
-
-    context.restore();
-}
-/*************************************Render End******************************************************/
+    /*************************************Render End******************************************************/
 
 /*************************************Sprite System Begin***************************************************/
-function BLFBaseSprite()
-{
-    if(!(this instanceof BLFBaseSprite))
+function BLFBaseSprite() {
+    if (!(this instanceof BLFBaseSprite))
         return new BLFBaseSprite();
 
     this.typeName = "BASE";
 }
 
-BLFBaseSprite.prototype.getTypeName = function()
-{
+BLFBaseSprite.prototype.getTypeName = function() {
     return this.typeName;
 }
 
-BLFBaseSprite.prototype.render = function(render)
-{
-    render.drawGrid('black','white',10,10);
+BLFBaseSprite.prototype.render = function(render) {
+    render.drawGrid('black', 'white', 10, 10);
 }
 
 /***************************Sprite System Rect ***********************************/
-function BLFRectSprite()
-{
-    if(!(this instanceof BLFRectSprite))
+function BLFRectSprite() {
+    if (!(this instanceof BLFRectSprite))
         return new BLFRectSprite();
 
     BLFBaseSprite.call(this);
@@ -92,6 +86,6 @@ function BLFRectSprite()
     this.typeName = "RECT";
 }
 
-BLFUtil.inherits(BLFRectSprite,BLFBaseSprite);
+BLFUtil.inherits(BLFRectSprite, BLFBaseSprite);
 
 /*************************************Sprite System End*****************************************************/
